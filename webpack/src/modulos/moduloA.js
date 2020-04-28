469d499c959371dab8ff725bd89665f6bfd37a91
Alex de Souza <alex.silva@7comm.com.br>
2020-04-28 04:36:34 -0300

    webpack

---
+++ webpack/src/modulos/moduloA.js
@@ -0,0 +1,8 @@
+const moduloB = require('./moduloB')
+
+console.log(moduloB.saudacao())
+
+/**
+ * c/Softwares/Java Script/Projetos/exercicios-web/webpack
+ * Comando: npm start && node  public/principal.js
+*/
---
+++ webpack/src/principal.js
@@ -0,0 +1,12 @@
+import Pessoa from './pessoa'
+//import './modulos/moduloA'
+import './assets'
+
+const atendente = new Pessoa
+console.log(atendente.comprimentar())
+
+/**
+ * Comando:
+ * npm start && node  public/principal.js
+ * npm run build
+*/
