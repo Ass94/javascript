4b76a2706851737e92b9828cfae121321beccf4f
Alex de Souza <alex.silva@7comm.com.br>
2020-05-18 05:10:29 -0300

    fim capítulo react

--- react/crud/frontend/src/components/template/Nav.jsx
+++ react/crud/frontend/src/components/template/Nav.jsx
@@ -1,8 +1,20 @@
 import './Nav.css'
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 // componente funcional
 export default props =>
     <aside className="menu-area">
-        Navegação
+        <nav className="menu">
+            {/* Refatorar em casa. A ideia é não ter elmentos repetidos 
+            1. Criar novo componente.
+            2. Importá-lo para cá
+            3. Passar os parâmetros*/}
+            <Link to="/">
+                <i className="fa fa-home"></i>Início
+            </Link>
+            <Link to="/users">
+                <i className="fa fa-users"></i> Usuários
+            </Link>
+        </nav>
     </aside>
