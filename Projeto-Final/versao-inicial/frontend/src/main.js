6c5bd935ab3a1231f015f952a29c22cf57949025
Alex de Souza <alex.silva@7comm.com.br>
2020-06-23 13:55:13 -0300

    finalização backend e início frontend

--- Projeto-Final/versao-inicial/frontend/src/main.js
+++ Projeto-Final/versao-inicial/frontend/src/main.js
@@ -1,9 +1,21 @@
+import 'font-awesome/css/font-awesome.css'
+
 import Vue from 'vue'
 
 import App from './App'
 
+import './config/bootstrap'
+import store from './config/store'
+import router from './config/router'
+
 Vue.config.productionTip = false
 
+// TEMPORÁRIO!
+require('axios').defaults.headers.common['Authorization'] = 'bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpZCI6MSwibmFtZSI6IkFsZXggZGUgU291emEiLCJlbWFpbCI6ImFsZXhAbmVnb2Npby5jb20uYnIiLCJhZG1pbiI6dHJ1ZSwiaWF0IjoxNTkyOTAxOTY3LCJleHAiOjE1OTMxNjExNjd9.zMzZ4BYlXO0j6ox1Itds8czOCX0avG789n-SmucTv_k'
+
+
 new Vue({
+  store,
+  router,
   render: h => h(App)
 }).$mount('#app')
